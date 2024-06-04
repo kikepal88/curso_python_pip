b38eeb352692bea57f26b18f36ed12e8d2b4fd0e
kikepal88 <omarpal88@gmail.com>
2024-06-03 19:11:58 -0500

    add pandas into app project and fastAPI into server project

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 import utils
-import utils
 import charts
+import pandas as pd
 
 def run():
-  data = utils.get_data('data.csv')
-  data = list(filter(lambda item : item['Continent'] == 'South America', data))
+  # data = utils.get_data('data.csv')
+  # data = list(filter(lambda item : item['Continent'] == 'South America', data))
 
-  countries = list(map(lambda x: x['Country/Territory'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  # countries = list(map(lambda x: x['Country/Territory'], data))
+  # percentages = list(map(lambda x: x['World Population Percentage'], data))
+  # charts.generate_pie_chart(countries, percentages)
+
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
   charts.generate_pie_chart(countries, percentages)
 
+  data = utils.get_data('data.csv')
   country = input('Type country => ')
 
   result = utils.population_by_country(data, country)
